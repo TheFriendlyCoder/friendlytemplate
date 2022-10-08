ab3a18b951fd630fbecf8ce72f990f3a7f9cccd9
Kevin Phillips <kevinp@ca.ibm.com>
2022-10-08 13:26:54 -0300

    Added JGit dependencies

--- app/src/main/java/friendlytemplate/app/App.java
+++ app/src/main/java/friendlytemplate/app/App.java
@@ -1,4 +1,5 @@
 package friendlytemplate.app;
+import org.eclipse.jgit.api.Git;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -7,8 +8,10 @@ import picocli.CommandLine.Parameters;
 import java.io.File;
 import java.math.BigInteger;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.util.concurrent.Callable;
+
 
 @Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
@@ -22,6 +25,16 @@ class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception { // your business logic goes here...
+        System.out.println("Checking out sample...");
+        String repo = "https://github.com/TheFriendlyCoder/friendlytemplate";
+        Path outPath = Files.createTempDirectory("friendlytemplate");
+        System.out.println("Output folder is " + outPath);
+        Git git = Git.cloneRepository()
+                .setURI(repo)
+                .setDirectory(outPath.toFile())
+                .call();
+        System.out.println(git.toString());
+
         byte[] fileContents = Files.readAllBytes(file.toPath());
         byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
         System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
