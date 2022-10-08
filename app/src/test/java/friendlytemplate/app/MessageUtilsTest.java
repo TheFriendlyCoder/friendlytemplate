6451f02efef85602795c21860f08bcfcbfd4f71b
Kevin Phillips <kevinp@ca.ibm.com>
2022-10-08 12:36:02 -0300

    converted build to groovy

--- app/src/test/java/friendlytemplate/app/MessageUtilsTest.java
+++ app/src/test/java/friendlytemplate/app/MessageUtilsTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MessageUtilsTest {
-    @Test
-    void testGetMessage() {
+    @Test void testGetMessage() {
         assertEquals("Hello      World!", MessageUtils.getMessage());
     }
 }
