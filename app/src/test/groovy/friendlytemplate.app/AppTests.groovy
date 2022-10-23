e97a99a7dc45692cdfabdb89ef240ebcccfd6e08
Kevin Phillips <kevinp@ca.ibm.com>
2022-10-23 15:05:12 -0300

    Converted tests to Spock framework

--- app/src/test/groovy/friendlytemplate.app/AppTests.groovy
+++ app/src/test/groovy/friendlytemplate.app/AppTests.groovy
@@ -1,8 +1,73 @@
+package friendlytemplate.app
+
+import picocli.CommandLine
 import spock.lang.Specification
+import spock.lang.TempDir
+import java.nio.file.Path
+
 
 class AppTests extends Specification {
-    def "My First Test"() {
-        expect:
-        "fu" == "fu"
+    @TempDir
+    Path tempDir
+
+    def "Show version output"() {
+        given:
+        String[] args = ["--version"]
+        CommandLine cmd = App.defaultCommandLineSpec(args)
+
+        StringWriter sw = new StringWriter()
+        cmd.setOut(new PrintWriter(sw))
+
+        when:
+        int exitCode = cmd.execute(args);
+
+        then:
+        exitCode == 0
+        // NOTE: the version number for the package currently doesn't get
+        // loaded properly from the app when it is run from within the local
+        // test environment
+        sw.toString().strip() == "friendlytemplate version null"
+    }
+
+    def "Process basic template"() {
+        given:
+        String templateDir = getClass().getClassLoader().getResource("simpleExample").getPath()
+        String[] args = [templateDir, tempDir.toString(), "--project_name=testproj"]
+
+        CommandLine cmd = App.defaultCommandLineSpec(args)
+        StringWriter sw = new StringWriter()
+        cmd.setOut(new PrintWriter(sw))
+
+        when:
+        int exitCode = cmd.execute(args)
+        String output = sw.toString()
+
+        then:
+        exitCode == 0
+        output.contains(templateDir)
+        output.contains(tempDir.toString())
+        output.contains("project_name")
+    }
+
+    void "Missing template folder"() {
+        given:
+        Path sourceDir = tempDir.resolve("source")
+        sourceDir.toFile().mkdir()
+        Path targetDir = tempDir.resolve("target")
+        targetDir.toFile().mkdir()
+
+        String[] args = [sourceDir.toString(), targetDir.toString()]
+        CommandLine cmd = App.defaultCommandLineSpec(args)
+        StringWriter stderr = new StringWriter()
+        cmd.setErr(new PrintWriter(stderr))
+
+        when:
+        int exitCode = cmd.execute(args)
+        String output = stderr.toString()
+
+        then:
+        exitCode == -1
+        output.contains("not found")
+        output.contains(sourceDir.toString())
     }
 }
---
+++ app/src/test/groovy/friendlytemplate.app/ConfigFileTests.groovy
@@ -0,0 +1,35 @@
+package friendlytemplate.app
+
+import spock.lang.Specification
+
+
+class ConfigFileTests extends Specification {
+    def "Read template version from config file"() {
+        when:
+        InputStream initialStream = new ByteArrayInputStream("templateVersion: 2".bytes)
+        ConfigFile cfg = ConfigFile.fromYaml(initialStream)
+
+        then:
+        cfg.getTemplateVersion() == 2
+    }
+
+    def "Read sample template from disk"() {
+        when:
+        InputStream sample_file = getClass().getClassLoader().getResourceAsStream("simpleExample/friendly.template.yml")
+        ConfigFile cfg = ConfigFile.fromYaml(sample_file)
+
+        then:
+        cfg.getTemplateVersion() == 1
+    }
+
+    def "Read field names from config file"() {
+        when:
+        InputStream sample_file = getClass().getClassLoader().getResourceAsStream("simpleExample/friendly.template.yml")
+        ConfigFile cfg = ConfigFile.fromYaml(sample_file)
+        List<String> values = cfg.getFieldNames()
+
+        then:
+        values.size() == 1
+        values[0] == "project_name"
+    }
+}
